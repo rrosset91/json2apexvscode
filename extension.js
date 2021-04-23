b3fef3073f53162a478274056314f6e450ab189a
Roger Rosset <roger.rosset@globant.com>
2021-04-23 13:49:05 -0300

    All set

--- extension.js
+++ extension.js
@@ -1,52 +1,84 @@
+const fetch = require("node-fetch");
 const vscode = require("vscode");
 const editor = vscode.window.activeTextEditor;
-const {spawn} = require('child_process');
+let outputTerminal = vscode.window.createOutputChannel("JSON2Apex");
 
 async function json2apex() {
   const userSelection = editor.document.getText(editor.selection);
-  const className = await vscode.window.showInputBox({
-    placeHolder: "Enter the generated class name"
-  });
-  const createTest = await vscode.window.showInputBox({
-    placeHolder: "Generate class with tests? (Y/N - Default N)"
-  });
-  
-  let params = ['script.py'];
-  if(className === ''){
-	  showMessage('error', 'Classname cannot be empty');
-	  return;
-	}
   if(userSelection === '' || isInvalidSelection(userSelection)){
 	  showMessage('error', 'Please select a valid JSON content and try again');
 	  return;
 	} 
-  createTest == '' ? 'N':'Y';	
-  params.push(className);
-  params.push(createTest);
-  params.push(userSelection);
 
-  let response = await callPython(params).catch((e)=>{
+  const className = await vscode.window.showInputBox({
+    placeHolder: "Enter the generated class name"
+  });
+  if(className === ''){
+	  showMessage('error', 'Classname cannot be empty');
+	  return;
+	}
+
+  let createTest = await vscode.window.showInputBox({
+    placeHolder: "Generate class with tests? (Y/N - Default N)"
+  });
+  createTest = createTest.toUpperCase();
+  createTest = createTest == '' ? false : createTest
+  createTest = createTest == 'Y' ? true : createTest
+
+  let auraEnabled = await vscode.window.showInputBox({
+    placeHolder: "Use @AuraEnabled? (Y/N - Default Y)"
+  });
+  auraEnabled = auraEnabled.toUpperCase();
+  auraEnabled = auraEnabled == '' ? true : auraEnabled
+  auraEnabled = auraEnabled == 'N' ? false : auraEnabled
+  
+  try {
+    isValidInput(auraEnabled, createTest); 
+  } catch (error) {
+    showMessage('error', error.message);
+    return;
+  }
+  let params = {};
+  outputTerminal.show();
+  createTest == '' ? 'N':createTest;	
+  auraEnabled == '' ? 'Y':auraEnabled;	
+  params.className = className;
+  params.generateTest = createTest;
+  params.auraEnabled = auraEnabled;
+  params.jsonContent = String(userSelection);
+  outputTerminal.appendLine('Process started.Please stand by...')
+  
+  submitForConversion(params).catch((e)=>{
 	  showMessage('error', `Something went wrong...${e.message}`);
   });
+}
+
+async function submitForConversion(params){
+  let response = await fetch(`https://json2apexpy.herokuapp.com/json2apex?className=${params.className}&generateTest=${params.generateTest}&jsonContent=${params.jsonContent}&auraEnabled=${params.auraEnabled}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    }).catch((e)=>{
+      showMessage('error', `Something went wrong...${e.message}`);
+    })
+  let treatedResponse = await response.json();
+  outputTerminal.appendLine('Response captured successfully...')
+  let replacementText;
+  if(params.generateTest){
+    replacementText = treatedResponse.wrapperClass+'\n\n'+treatedResponse.testClass;
+  }else{
+    replacementText = treatedResponse.wrapperClass
+  }
+  outputTerminal.appendLine('Converting data...')
   if (editor) {
-	editor.edit((selectedText) => {
-		selectedText.replace(editor.selection, response);
-	})
-}
-}
-
-async function callPython(params){
-	let dataToSend;
-
-	const python = spawn('python', params);
-	python.stdout.on('data', function (data) {
-	 dataToSend = data.toString();
-	});
-	
-	python.on('close', () => {
-	let response = dataToSend;
-	return response;
-	});
+    editor.edit((selectedText) => {
+      selectedText.replace(editor.selection, replacementText);
+    })
+    outputTerminal.appendLine('Done: !JSON replaced with the generated wrapper class content!')
+    showMessage('success', 'JSON conversion completed successfully');
+  }
 }
 
 function showMessage(context, content){
@@ -74,8 +106,24 @@ function isInvalidSelection(input){
     return false;
 }
 
+function isValidInput(aura, test){
+  let validAura = false;
+  let validTest = false;
+  if(aura === true || aura === false){
+    validAura = true;
+  }
+  if(test === true || test === false){
+    validTest = true;
+  }
+  if(!validAura){
+    throw new Error('Invalid option for auraEnabled, please use Y, N or Enter to Default');
+  }
+  if(!validTest){
+    throw new Error('Invalid option for create test class, please use Y, N or Enter to Default');
+  }
+}
+
 function activate(context) {
-  // this code runs whenever your click 'Create Gist' from the context menu in your browser.
   let disposable = vscode.commands.registerCommand(
     "extension.json2apex",
     function() {
